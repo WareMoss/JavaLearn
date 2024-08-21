6b4e0b5e0e06758211d7d6b7709d0e09e1e76dc0
Ryan <115543058+WareMoss@users.noreply.github.com>
2024-08-21 13:04:38 +0100

    Create loopsyntax.java
    Added the do loop syntax

---
+++ loopsyntax.java
@@ -0,0 +1,13 @@
+public class loopsntax{
+  public static void main(String[] args){
+  }
+class dowhile{
+    public static void doloop(){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<5);
+      }
+  }
+}
